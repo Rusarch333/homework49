b1147fa58c5b17905017375c30d8f8133f0203ee
Lite <rusarch333@gmail.com>
2024-05-20 23:41:06 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+  Написати функцію createAdder, яка в якості параметра стартове значення для накопичення(наприклад 10) і повертає нову функцію, яка приймає в якості параметра наступне значення(наприклад 5) і повертає результат накопичення (додавати до попереднього нове значення (10+5)).
+*/
+
+const createAdder = (counter) => (additionalValue) =>
+  (counter += additionalValue);
+
+const adder = createAdder(10);
+const adder2 = createAdder(100);
+
+// adder
+console.log(adder(5)); // 15
+console.log(adder(3)); // 18
+console.log(adder(4), "\n"); // 22
+
+// adder 2
+console.log(adder2(5)); // 105
+console.log(adder2(3)); // 108
+console.log(adder2(104)); // 212
